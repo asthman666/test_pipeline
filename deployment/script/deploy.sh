5b9ee7c90496afe72e7f2bef36bd8077333c5222
asthman <414295467@qq.com>
2022-12-12 00:06:06 +0800

    fix path

--- deployment/script/deploy.sh
+++ deployment/script/deploy.sh
@@ -1,8 +1,8 @@
 set -exo pipefail
 
 echo "Deploy begin"
-kubectl version --client
-kubectl apply -f ../eks-sample-deployment.yaml
-kubectl apply -f ../eks-sample-service.yaml
-kubectl apply -f ../eks-sample-ingress.yaml
+#echo `pwd`
+kubectl apply -f ./deployment/eks-sample-deployment.yaml
+kubectl apply -f ./deployment/eks-sample-service.yaml
+kubectl apply -f ./deployment/eks-sample-ingress.yaml
 echo "Deploy end"
