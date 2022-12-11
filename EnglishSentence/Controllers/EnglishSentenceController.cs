d2e5fa27f362cc6e9830ea1dba023e60adf3f535
asthman <414295467@qq.com>
2022-12-11 09:42:02 +0800

    fix sentence

--- EnglishSentence/Controllers/EnglishSentenceController.cs
+++ EnglishSentence/Controllers/EnglishSentenceController.cs
@@ -12,9 +12,7 @@ namespace EnglishSentence.Controllers
             "The only limit to the height of your achievements is the reach of your dreams and your willingness to work hard for them.",
             "A leader has the vision and conviction that a dream can be achieved. He inspires the power and energy to get it done.",
             "It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.",
-            "It’s not that I’m so smart, it’s just that I stay with problems longer.",
             "Optimists always picture themselves accomplishing their goals.",
-            "I’ve always had confidence. It came because I have lots of initiative. I wanted to make something of myself."
         };
 
         private readonly ILogger<EnglishSentenceController> _logger;
