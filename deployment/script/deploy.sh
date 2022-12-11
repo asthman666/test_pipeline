6aa80ed05b5b8afb698f329e12e0d35c57b5090c
asthman <414295467@qq.com>
2022-12-11 23:47:20 +0800

    add version for image

--- deployment/script/deploy.sh
+++ deployment/script/deploy.sh
@@ -2,6 +2,7 @@ set -exo pipefail
 
 echo "Deploy begin"
 kubectl version --client
-#kubectl apply -f ../eks-sample-deployment.yaml
-#kubectl apply -f ../eks-sample-service.yaml
+kubectl apply -f ../eks-sample-deployment.yaml
+kubectl apply -f ../eks-sample-service.yaml
+kubectl apply -f ../eks-sample-ingress.yaml
 echo "Deploy end"
