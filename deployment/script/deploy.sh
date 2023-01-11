6ba9297d8ca71275392542cae5d268d56c77a776
asthman <414295467@qq.com>
2023-01-11 23:39:58 +0800

    Update deploy.sh

--- deployment/script/deploy.sh
+++ deployment/script/deploy.sh
@@ -2,10 +2,8 @@
 set -exo pipefail
 
 echo "Deploy begin"
-BUILDKITE_BUILD_NUMBER="${BUILDKITE_BUILD_NUMBER}"
-echo $BUILDKITE_BUILD_NUMBER
-#echo `pwd`
-cat ./deployment/eks-sample-deployment.yaml | sed "s/{{BUILDKITE_BUILD_NUMBER}}/$BUILDKITE_BUILD_NUMBER/g" | kubectl apply -f -
+BUILDKITE_TRIGGERED_FROM_BUILD_NUMBER="${BUILDKITE_TRIGGERED_FROM_BUILD_NUMBER}"
+cat ./deployment/eks-sample-deployment.yaml | sed "s/{{BUILDKITE_TRIGGERED_FROM_BUILD_NUMBER}}/$BUILDKITE_TRIGGERED_FROM_BUILD_NUMBER/g" | kubectl apply -f -
 kubectl apply -f ./deployment/eks-sample-service.yaml
 #kubectl apply -f ./deployment/eks-sample-ingress.yaml
 echo "Deploy end"
