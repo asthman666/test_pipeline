fb94893ed7a657289a7d72e53804e6e0a460f1a4
asthman <414295467@qq.com>
2023-01-11 23:27:15 +0800

    remove ingress

--- deployment/script/deploy.sh
+++ deployment/script/deploy.sh
@@ -7,5 +7,5 @@ echo $BUILDKITE_BUILD_NUMBER
 #echo `pwd`
 cat ./deployment/eks-sample-deployment.yaml | sed "s/{{BUILDKITE_BUILD_NUMBER}}/$BUILDKITE_BUILD_NUMBER/g" | kubectl apply -f -
 kubectl apply -f ./deployment/eks-sample-service.yaml
-kubectl apply -f ./deployment/eks-sample-ingress.yaml
+#kubectl apply -f ./deployment/eks-sample-ingress.yaml
 echo "Deploy end"
